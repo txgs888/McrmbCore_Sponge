abc2ffe79878821a919d82491c7d4dcd7e237816
txgs888 <txgs888@gmail.com>
2017-04-29 20:04:20 +0800

    添加注释

--- src/main/java/com/mcrmb/sponge/command/CommandHandler.java
+++ src/main/java/com/mcrmb/sponge/command/CommandHandler.java
@@ -7,12 +7,35 @@ import org.spongepowered.api.command.args.CommandContext;
  * Created by txgs888 on 2017/4/29.
  */
 public interface CommandHandler {
+    /**
+     * 获取子命令名字
+     *
+     * @return 子命令名字
+     */
     String getName();
 
+    /**
+     * 获取子命令介绍
+     *
+     * @return 介绍文本
+     */
     String getDescribe();
 
+    /**
+     * 判断命令来源是否有该命令权限
+     *
+     * @param source 命令来源
+     * @return 是否拥有权限
+     */
     boolean hasPermission(CommandSource source);
 
+    /**
+     * 执行命令
+     *
+     * @param source  命令来源
+     * @param context 命令参数
+     * @return 是否执行成功
+     */
     boolean execute(CommandSource source, CommandContext context);
 
 }
